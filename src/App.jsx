c4289fb632dfb19b5a6c70b5249fb1bfa6571580
Gokulr1503 <gokulrathinasamy1503@gmail.com>
2024-11-30 11:58:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./index.css";
+import Todo from "./components/Todo";
+
+export default function App() {
+  return (
+    <div className="bg-slate-800 grid py-4 h-screen">
+      <Todo />
+    </div>
+  );
+}
